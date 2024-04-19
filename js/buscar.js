82c1670e28a8858fd6ba6d59162f71d4ae7da5af
Guilherme Gonçalves <125297568+guigon14@users.noreply.github.com>
2024-04-19 10:39:00 -0300

    Atualizacao bootcamp

--- js/buscar.js
+++ js/buscar.js
@@ -1,20 +1,50 @@
-
-
 async function buscar() {
+    console.log("Passei por aqui");
 
-    console.log("Passei por aqui")
-    const resposta = await fetch("https://660f44af356b87a55c510d68.mockapi.io/agendas")
+    const resposta = await fetch("https://660f44b1356b87a55c510dd2.mockapi.io/agendas");
+
     const respostaDadosAgenda = await resposta.json();
-    console.log("Resposta", respostaDadosAgenda);
-   
-    
-    const cards = document.getElementById("cards_games")
-    
-    cards.innerHTML = respostaDadosAgenda.map(
-        (itemAgenda) => {    
-            return "TESTE"
-        }
-    )
-}
-buscar();
+    console.log("Resposta: ", respostaDadosAgenda);
 
+    const cards = document.getElementById("cards_games");
+
+    cards.innerHTML = respostaDadosAgenda.map(
+        (itemAgenda) => {
+            return `
+            <div class="cardItem">
+            <div class="dataGame"> 
+                <img src="../imagens/calendar-solid.svg" alt="">${itemAgenda.dataJogo}
+            </div>
+            <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+            <div class="descricao">
+                <p> <strong>${itemAgenda.nome}</strong> </p>
+                <p>${itemAgenda.descricao}</p>
+                <p><strong>Gamers: </strong></p>
+
+
+                <div class="gamers">
+                   ${
+                    itemAgenda.gamers.map(
+                        (gamerItem) => {
+                            return `
+                            <div class="gamerItem">${gamerItem}</div>
+                            `
+                        }).join("")
+                   }
+                </div>
+
+              
+            </div>  
+            <div class="assistir">
+                    <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+            </div>
+        </div>
+        `
+        }
+    );
+
+}
+
+console.log("Antes de buscar");
+buscar();
+console.log("Depois de buscar");
